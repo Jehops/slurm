21eff8fecddc3020f209d4ba0d3dbca87a9b2722
Joseph Mingrone <jrm@ftfl.ca>
2018-09-30 03:03:01 -0300

    srun_job: Fix FreeBSD build by defining environ and __environ
    1. glibc has `extern char **environ;` in <unistd.h>, but hidden behind `#ifdef
    __USE_GNU`.  Since libc has the `environ` symbol defined, simply defining it
    here should work.  Guard it behind #ifdef __FreeBSD__ to ensure this does not
    cause problems elsewhere.
    
    2. #define __environ environ

--- src/srun/libsrun/srun_job.c
+++ src/srun/libsrun/srun_job.c
@@ -113,6 +113,11 @@ static int shepherd_fd = -1;
 static pthread_t signal_thread = (pthread_t) 0;
 static int pty_sigarray[] = { SIGWINCH, 0 };
 
+#ifdef __FreeBSD__
+#define __environ environ
+extern char **environ;
+#endif
+
 /*
  * Prototypes:
  */
